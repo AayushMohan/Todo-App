631f6ebc9ceeec472fb6dc3b5ed76ceeb881af59
AayushMohan <aayushmohan1702@gmail.com>
2022-01-15 21:26:07 +0530

    commit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -17,7 +17,7 @@ const FormControl = styled.div`
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (invalid ? "red" : "#ccc")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -63,7 +63,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && "invalid"}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
